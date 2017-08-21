f769e6d5b21d89346386ed1e0e10069012072fec
Parv Jain <parv.wutizup@gmail.com>
2017-08-21 15:54:47 +0530

    API for adding employee

--- python/main.py
+++ python/main.py
@@ -25,6 +25,7 @@ class Employee(db.Model):
     __tablename__ = "employee"
     id = db.Column(db.Integer, primary_key=True)
     name = db.Column(db.String(56))
+    role = db.Column(db.String(128))
     birth_date = db.Column(db.Date)
     joining_date = db.Column(db.Date)
     address = db.Column(db.Text())
@@ -36,6 +37,7 @@ class Employee(db.Model):
         return {
             'id': self.id,
             'name': self.name,
+            'role': self.role,
             'birth_date': self.birth_date,
             'joining_date': self.joining_date,
             'age': int((datetime.date.today() - self.birth_date).days / 365.25),
@@ -45,10 +47,24 @@ class Employee(db.Model):
 
 @app.route('/employees/all', methods = ['GET'])
 def index():
-    # employee_schema = jsonify(employees = )
     print [e.serialize() for e in Employee.query.all()]
     return jsonify({'employees': [e.serialize() for e in Employee.query.all()]})
 
+@app.route('/employees/create', methods = ['POST'])
+def create_employee():
+    print request.json
+    if not request.json or not 'name' in request.json:
+        abort(400)
+    from datetime import datetime
+    employee = Employee(name=request.json.get('name'),
+                        role=request.json.get('role', ''),
+                        birth_date=request.json.get('birth_date',''),
+                        joining_date=request.json.get('joining_date', datetime.now()),
+                        address=request.json.get('address', ''),
+                        parent_id=request.json.get('parent_id', ''))
+    db.session.add(employee)
+    db.session.commit()
+    return jsonify( { 'employee': employee.serialize() } ), 201
 
 if __name__ == '__main__':
     admin = Admin(app)
